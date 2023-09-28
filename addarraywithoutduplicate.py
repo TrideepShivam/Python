7de0374d8a8d52ab16d12c348af7a02e00dbb1df
trideepshivam <trideepshivam@gmail.com>
2023-09-28 11:52:46 +0530

    union array

---
+++ addarraywithoutduplicate.py
@@ -0,0 +1,4 @@
+import numpy as np
+num1 = np.array([1,5,4,2,3,9])
+num2= np.array([11,15,14,12,13,19])
+print(np.union1d(num1,num2))
--- numpyreverse.py
+++ numpyreverse.py
@@ -1,5 +1,5 @@
 import numpy as np
-nums = np.array([[1,5,4,2,3,9],[1,5,4,2,3,9]])
+nums = np.array([[1,5,4,2,3,9],[11,15,14,12,13,19]])
 print(nums)
 print(np.flip(nums))
 print("after flipp")
