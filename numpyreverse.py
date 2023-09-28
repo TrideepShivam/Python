2856cea8c36806902e5601632b6d7d4a0b4fe42a
trideepshivam <trideepshivam@gmail.com>
2023-09-28 11:31:52 +0530

    array reverse in numpy

---
+++ numpyreverse.py
@@ -0,0 +1,6 @@
+import numpy as np
+nums = np.array([[1,5,4,2,3,9],[1,5,4,2,3,9]])
+print(nums)
+print(np.flip(nums))
+print("after flipp")
+print(nums[::-1])
--- replaceNum.py
+++ replaceNum.py
@@ -1,7 +1,9 @@
 import numpy as np
 nums = np.array([1,5,4,2,3,9])
 print(nums)
+
 nums[nums==2]=0
 print(nums)
 nums[nums>4]=8
 print(nums)
+
